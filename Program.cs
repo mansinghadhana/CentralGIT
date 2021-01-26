72d62ea7e28837cac1006e85df7603de05a86eb0
ManSingh <mansinghadhana@gmail.com>
2021-01-26 23:08:19 +0530

    add console line

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace code
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! Hello India");
+            Console.WriteLine("Hello World! Hello India1");
+            Console.WriteLine("Hello World! Hello India2");
         }
     }
 }
